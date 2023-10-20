9d6707026897269c6db19832620b96e231349df4
Antonin Masopust <purple.dragon@email.cz>
2023-10-20 08:57:09 +0200

    Enabled enemy spawner, enemies now look toward player

--- Assets/Scripts/Game/Enemy.cs
+++ Assets/Scripts/Game/Enemy.cs
@@ -74,6 +74,14 @@ public class Enemy : MonoBehaviour
          *    Use mRigidBody.MovePosition to move the enemy
          * Implement a simple AI, which will head towards the closest player and follow them.
          */
+
+        GameObject nearestPlayer = GameManager.Instance.NearestPlayer(transform.position);
+        if (nearestPlayer != null)
+        {
+            Vector3 direction = nearestPlayer.transform.position - transform.position;
+            Quaternion rot = Quaternion.LookRotation(direction, Vector3.forward);
+            transform.rotation = rot;
+        }
     }
 
     /// <summary>
